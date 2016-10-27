e839b8339d46de8609c960e16d531497777ca021
Jon Willemain <jon.willemain@gmail.com>
2016-10-27 11:40:26 -0600

    add final modifications to button and input hover, outline, padding, spacing

--- script.js
+++ script.js
@@ -1,122 +1,122 @@
 $(document).ready(function (){
-  for (var i=0; i<localStorage.length; i++){
-    var object = JSON.parse(localStorage.getItem(localStorage.key(i)));
-    createIdea(object);
-  }
+ for (var i=0; i<localStorage.length; i++){
+   var object = JSON.parse(localStorage.getItem(localStorage.key(i)));
+   createIdea(object);
+ }
 });
 
 
 function createIdea(idea) {
-  $('.render-idea').prepend(
-    `<li class ='idea-box' id=${idea.id}>
-        <h2 class='title-result' contenteditable>${idea.title}</h2>
-        <p class='body-result' contenteditable>${idea.body}</p>
-        <span display='inline'>quality: <p class='quality'>${idea.quality}</p></span>
-        <button class='delete-button'>delete
-        </button>
-        <button class='upvote'>Up</button>
-        <button class='downvote'>Down</button>
-      </li>`);
+ $('.render-idea').prepend(
+   `<li class ='idea-box' id=${idea.id}>
+       <h2 class='title-result' contenteditable>${idea.title}<button class='delete-button'></button></h2>
+       <p class='body-result' contenteditable>${idea.body}</p>
+       <button class='upvote'></button>
+       <button class='downvote'></button>
+       <span class='quality-text'>quality: <span class='quality'>${idea.quality}</span></span>
+     </li>`);
 };
 
 function Idea(title, body){
-  this.title = title;
-  this.body = body;
-  this.id = Date.now()
-  this.quality = 'swill'
+ this.title = title;
+ this.body = body;
+ this.id = Date.now()
+ this.quality = 'swill'
 }
 
 $('.save-button').on('click', function(){
-  var title = $('.title-input').val();
-  var body = $('.body-input').val();
-  var idea = new Idea(title, body);
-  createIdea(idea);
-  localStorage.setItem(idea.id, JSON.stringify(idea));
+ var title = $('.title-input').val();
+ var body = $('.body-input').val();
+ var idea = new Idea(title, body);
+ createIdea(idea);
+ localStorage.setItem(idea.id, JSON.stringify(idea));
+ $('.title-input').val('');
+ $('.body-input').val('');
 });
 
 $('.render-idea').on('click', '.delete-button', function(){
-  var idNumber = $(this).closest('li').attr('id');
-  localStorage.removeItem(idNumber);
-  $(this).closest('li').remove();
+ var idNumber = $(this).closest('li').attr('id');
+ localStorage.removeItem(idNumber);
+ $(this).closest('li').remove();
 });
 
 $('.render-idea').on('click', '.upvote, .downvote', function(){
-  var parentSelector = $(this).closest('li');
-  var idNumber = parentSelector.attr('id');
-  var quality = parentSelector.find('.quality').text();
-  var upOrDown = $(this).attr('class');
-  var newQuality = buttonSort(upOrDown, quality);
-  parentSelector.find('.quality').text(newQuality);
-  var ideaBox = JSON.parse(localStorage.getItem(idNumber));
-  ideaBox.quality = newQuality;
-  localStorage.setItem(idNumber, JSON.stringify(ideaBox));
+ var parentSelector = $(this).closest('li');
+ var idNumber = parentSelector.attr('id');
+ var quality = parentSelector.find('.quality').text();
+ var upOrDown = $(this).attr('class');
+ var newQuality = buttonSort(upOrDown, quality);
+ parentSelector.find('.quality').text(newQuality);
+ var ideaBox = JSON.parse(localStorage.getItem(idNumber));
+ ideaBox.quality = newQuality;
+ localStorage.setItem(idNumber, JSON.stringify(ideaBox));
 });
 
 function upVote(quality){
-  switch(quality){
-    case 'swill':
-      return 'plausible';
-    case 'plausible':
-      return 'genius';
-    default:
-      return 'genius';
-    };
+ switch(quality){
+   case 'swill':
+     return 'plausible';
+   case 'plausible':
+     return 'genius';
+   default:
+     return 'genius';
+   };
 };
 
 function downVote(quality){
-  switch(quality){
-    case 'genius':
-      return 'plausible';
-    case 'plausible':
-      return 'swill';
-    default:
-      return 'swill';
-  };
+ switch(quality){
+   case 'genius':
+     return 'plausible';
+   case 'plausible':
+     return 'swill';
+   default:
+     return 'swill';
+ };
 };
 
 function buttonSort(upOrDown, quality){
-  if (upOrDown === 'upvote'){
-    return upVote(quality);
-  } else {
-    return downVote(quality);
-  };
+ if (upOrDown === 'upvote'){
+   return upVote(quality);
+ } else {
+   return downVote(quality);
+ };
 };
 
 $('.render-idea').on('focus', '.body-result, .title-result', function(){
-  var idNumber = $(this).closest('li').attr('id');
-  $(this).on('keypress', function(event){
-    if(event.keyCode === 13){
-      event.preventDefault();
-      $(this).blur();
-      return false;
-    }
-  })
-  $(this).on('blur', function(){
-    var object = JSON.parse(localStorage.getItem(idNumber));
-    object.title = $(this).closest('li').find('.title-result').text();
-    object.body = $(this).closest('li').find('.body-result').text();
-    localStorage.setItem(idNumber, JSON.stringify(object));
-  })
+ var idNumber = $(this).closest('li').attr('id');
+ $(this).on('keypress', function(event){
+   if(event.keyCode === 13){
+     event.preventDefault();
+     $(this).blur();
+     return false;
+   }
+ })
+ $(this).on('blur', function(){
+   var object = JSON.parse(localStorage.getItem(idNumber));
+   object.title = $(this).closest('li').find('.title-result').text();
+   object.body = $(this).closest('li').find('.body-result').text();
+   localStorage.setItem(idNumber, JSON.stringify(object));
+ })
 });
 
 $('.title-input, .body-input').keyup( function(){
-  var titleInput = $('.title-input').val();
-  var bodyInput = $('.body-input').val();
-  var saveButton = $('.save-button');
-  if (titleInput != '' && bodyInput != ''){
-    saveButton.attr('disabled', false);
-  } else {
-    saveButton.attr('disabled', true);
-  }
+ var titleInput = $('.title-input').val();
+ var bodyInput = $('.body-input').val();
+ var saveButton = $('.save-button');
+ if (titleInput != '' && bodyInput != ''){
+   saveButton.attr('disabled', false);
+ } else {
+   saveButton.attr('disabled', true);
+ }
 })
 
 
 $('.search').keyup(function() {
-  event.preventDefault();
-  var searchFilter = $(this).val().toLowerCase();
-  var uncorrectFilteredIdeas = $( "li:not(:contains(" + searchFilter + "))" );
-  var filteredIdeas = $("li:contains(" + searchFilter + ")"
-  );
-  filteredIdeas.show();
-  uncorrectFilteredIdeas.hide();
+ event.preventDefault();
+ var searchFilter = $(this).val().toLowerCase();
+ var uncorrectFilteredIdeas = $( "li:not(:contains(" + searchFilter + "))" );
+ var filteredIdeas = $("li:contains(" + searchFilter + ")"
+ );
+ filteredIdeas.show();
+ uncorrectFilteredIdeas.hide();
 });
