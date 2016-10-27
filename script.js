8994be36da93122d3b092f03faabc180ef614175
Noah Peden <noahpeden@gmail.com>
2016-10-26 21:59:22 -0600

    add upvote/downvote quality

--- script.js
+++ script.js
@@ -39,3 +39,45 @@ $('.render-idea').on('click', '.delete-button', function(){
   localStorage.removeItem(idNumber);
   $(this).closest('li').remove();
 });
+
+$('.render-idea').on('click', '.upvote, .downvote', function(){
+  var parentSelector = $(this).closest('li');
+  var idNumber = parentSelector.attr('id');
+  var quality = parentSelector.find('.quality').text();
+  var upOrDown = $(this).attr('class');
+  var newQuality = buttonSort(upOrDown, quality);
+  parentSelector.find('.quality').text(newQuality);
+  var ideaBox = JSON.parse(localStorage.getItem(idNumber));
+  ideaBox.quality = newQuality;
+  localStorage.setItem(idNumber, JSON.stringify(ideaBox));
+});
+
+function upVote(quality){
+  switch(quality){
+    case 'swill':
+      return 'plausible';
+    case 'plausible':
+      return 'genius';
+    default:
+      return 'genius';
+    }
+};
+
+function downVote(quality){
+  switch(quality){
+    case 'genius':
+      return 'plausible';
+    case 'plausible':
+      return 'swill';
+    default:
+      return 'swill';
+  }
+}
+
+function buttonSort(upOrDown, quality){
+  if (upOrDown === 'upvote'){
+    return upVote(quality);
+  } else {
+    return downVote(quality);
+  }
+}
